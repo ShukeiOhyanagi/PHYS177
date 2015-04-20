6d16fe7189373adf7776f27b551001a8d8940580
ShukeiOhyanagi <ShukeiOhyanagi@users.noreply.github.com>
2015-04-20 04:00:39 -0700

    Update ex1.py

--- Week02/ex1.py
+++ Week02/ex1.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import math
 
@@ -10,20 +11,27 @@ def TrapeRule(f,a,b,N):
 	Ip = 0.5*I*h
 	return Ip
 
-def SimpRule(f,h):
-	a = np.array(h)
-	b = np.array(-h)
-	I = 1. / 3. * h * (f(b)+4*f(0)+f(a))
-	return I
+def SimpRule(f,a,b,N):
+	h = (b-a) / float(N)
+	I = f(a) + f(b)
+	for k in range(1,N):
+		if k %2 == 0:
+			I1 = 2*f(a + k*h)
+			I = I + I1
+		else:
+			I2 = 4*f(a + k*h) 
+			I = I + I2
+	Ip = 1. / 3. * h * I
+	return Ip
 
 def f(x):
-	return 2*x**2 + 3*x + 4
+	return 2*x**2 + 3*x + 4 
 
 
-a = -5.
+a = 0.
 b = 5.
 N = 10
-h = 5.0
 
 print TrapeRule(f,a,b,N)
-print SimpRule(f,h)
+print SimpRule(f,a,b,N)
+
